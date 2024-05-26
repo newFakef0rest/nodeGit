506317d0f98fc4a5c99285a28d8432b1b6092673
newFakeF0rest <azizmamadjaprio@gmail.com>
2024-05-26 13:07:26 +0200

    Commiting first RESTApi with MongoDB

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+const { mongoose } = require("mongoose");
+const app = express();
+
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("connected to database"));
+
+app.use(express.json());
+
+const subscribersRouter = require("./routes/subscribers");
+app.use("/subscribers", subscribersRouter);
+
+app.listen(5000, () => {
+  console.log("server started");
+});
